1532ffbd67036348e93c3c425dd33cbe7b05fc58
nehapatil31 <nehapatil317@gmail.com>
2024-05-24 09:25:12 +0530

    Refactor assigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -3,39 +3,31 @@ import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import toast, { Toaster } from "react-hot-toast";
 
 const AssineeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
-    retry: 3,
-    staleTime: 1000 * 60 * 5, // 5 minutes
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />;
   if (error) return null;
+
+  const assignIssue = (userId: string) => {
+    axios
+      .patch(`/api/issues/${issue.id}`, {
+        assignedToUserId: userId == "nouser" ? null : userId,
+      })
+      .catch(() => {
+        toast.error("Failed to assign user");
+      });
+  };
 
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || "nouser"}
-        onValueChange={(userId) => {
-          axios
-            .patch(`/api/issues/${issue.id}`, {
-              assignedToUserId: userId == "nouser" ? null : userId,
-            })
-            .catch(() => {
-              toast.error("Failed to assign user");
-            });
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
@@ -54,5 +46,13 @@ const AssineeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
+    retry: 3,
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  });
 
 export default AssineeSelect;
