99515225cf5856567af4df5ad97389be3b4a14a3
nehapatil31 <nehapatil317@gmail.com>
2024-05-27 10:58:02 +0530

    fix build issue

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,13 @@
-"use client";
-import "easymde/dist/easymde.min.css";
 import dynamic from "next/dynamic";
 import IssueFormSkeleton from "./loading";
 
-export const IssueForm = dynamic(
-  () => import("@/app/issues/_components/IssueForm"),
-  { ssr: false, loading: () => <IssueFormSkeleton /> }
-);
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 const NewIssuePage = () => {
-  return (
-    <>
-      <IssueForm />
-    </>
-  );
+  return <IssueForm />;
 };
 
 export default NewIssuePage;
